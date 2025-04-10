fd82f190fd8f86400233e4e4183b624fd36ffd98
artinapira <artinapira2004@gmail.com>
2025-04-10 17:07:51 +0200

    Added frontend

--- Auto-Schedule/API/Program.cs
+++ Auto-Schedule/API/Program.cs
@@ -13,7 +13,19 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
