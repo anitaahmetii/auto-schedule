aa93f8094d127bea8a0566b9790dcadce4298843
nitaemini <emininita3@gmail.com>
2025-04-19 20:15:58 +0200

    Add form and service for Report module

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import StateTable from "./Components/StateTable";
 import EditState from "./Components/EditState";
+import ReportTable from "./Components/ReportTable";
+import EditReport from "./Components/EditReport";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -11,6 +13,9 @@ function App() {
      <Route path="/" element={<StateTable />} />
      <Route path="/EditState/:id" element={<EditState />} />
      <Route path="/AddState" element={<EditState />} />
+     <Route path="/reports" element={<ReportTable />} />
+     <Route path="/EditReport/:id" element={<EditReport />} />
+     <Route path="/AddReport" element={<EditReport />} />
      </Routes>
      </Router>
      </>
