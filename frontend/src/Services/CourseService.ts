871cf78eba5b7239723d0585ad785debc0103468
anitaahmetii <aniita.ahmeti@gmail.com>
2025-04-19 14:04:10 +0200

    Completed frontend CRUD functionality for Course management.

---
+++ frontend/src/Services/CourseService.ts
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { CourseModel } from "../Interfaces/CourseModel";
+
+export class CourseService
+{
+    private static readonly baseURL = "https://localhost:7085/api/Course";
+
+    public static async createCourseAsync(model: CourseModel): Promise<CourseModel>
+    {
+        try 
+        {
+            const response = await axios.post(`${CourseService.baseURL}`, model);
+            return response.data;
+        } 
+        catch (error) 
+        {
+            console.error("Error creating course:", error);
+            throw error;
+        }
+    }
+    public static async updateCourseAsync(model: CourseModel): Promise<CourseModel> 
+    {
+        try 
+        {
+          const response = await axios.put(`${CourseService.baseURL}/${model.id}`, model);
+          return response.data;
+        } 
+        catch (error) 
+        {
+          console.error("Error updating course:", error);
+          throw error;
+        }
+    }
+    public static async getByIdCourseAsync(id: string): Promise<CourseModel>
+    {
+        try
+        {
+            const response = await axios.get(`${CourseService.baseURL}/${id}`);
+            return response.data;
+        }
+        catch (error) 
+        {
+          console.error("Error retrieving the course:", error);
+          throw error;
+        }
+    }
+    public static async getAllCoursesAsync(): Promise<CourseModel[]>
+    {
+        try 
+        {
+            const response = await axios.get(`${CourseService.baseURL}`);
+            console.log(response);
+            return response.data;
+        }
+        catch (error) 
+        {
+          console.error("Error retrieving the courses:", error);
+          throw error;
+        }
+    }
+    public static async deleteCourseAsync(id: string): Promise<CourseModel>
+    {
+        try
+        {
+            const response = await axios.delete(`${CourseService.baseURL}/${id}`);
+            return response.data;
+        }
+        catch (error) 
+        {
+            console.error("Error deleting the course:", error);
+            throw error;
+        }
+    }
+}
